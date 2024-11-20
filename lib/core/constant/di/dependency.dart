13ec86ce0a57312458739f98c5e9240020bb0a82
junaeidy <junaeidys0@gmail.com>
2024-11-20 18:38:26 +0700

    setup network

--- lib/core/constant/di/dependency.dart
+++ lib/core/constant/di/dependency.dart
@@ -1,11 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:timely_app/core/network/app_interceptor.dart';
 
 final sl = GetIt.instance;
 
 Future<void> initDependency() async {
   Dio dio = Dio();
+  dio.interceptors.add(AppInterceptor());
   dio.interceptors.add(PrettyDioLogger(
       requestBody: true,
       requestHeader: true,
